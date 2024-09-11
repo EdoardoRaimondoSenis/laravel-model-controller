9c8bf7807cceca7f440bf6f5c5a8dac62718b746
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-09-11 13:59:23 +0200

    Template base

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\PageController as PageController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $title = 'Layout base - HOME';
+    return view('home', compact('title'));
+})->name('home');
+
+Route::get('/chi-siamo', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contatti', function () {
+    return view('contacts');
+})->name('contacts');
+
+Route::get('/', [PageController::class, 'index']);
